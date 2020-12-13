fa81ea7c419f62991fdb3e5c09fbfad5a49f1707
shch <shch91@126.com>
2020-12-14 00:02:24 +0800

    go

--- shch/fds.go
+++ shch/fds.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	for a := 1; a < 10; a++ {
 		for b := 1; b <= a; b++ {
-			fmt.Printf("%d * %d =%d \t", a, b, a*b)
+			fmt.Printf("%d * %d =%d  ", b, a, a*b)
 		}
 		fmt.Println()
 	}
