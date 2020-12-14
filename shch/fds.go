11e1409f27b6d4d8b5a7026526bae07f9a2cee6a
shch <shch91@126.com>
2020-12-14 15:02:08 +0800

    submit

--- shch/fds.go
+++ shch/fds.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math"
 )
+
+func Sqrt(f float64) (float64, error) {
+	if f < 0 {
+		return 0, errors.New("math: square root of negative number")
+	}
+	// 实现
+	return math.Sqrt(f),nil
+}
 
 func main() {
 	for a := 1; a < 10; a++ {
@@ -11,4 +21,11 @@ func main() {
 		}
 		fmt.Println()
 	}
+	var a int = 10
+	fmt.Printf("变量的地址: %x\n", &a)
+	var result,err=Sqrt(3213)
+	if err==nil{
+		fmt.Println(result)
+	}
+
 }
