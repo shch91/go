ca1dc6dc781a4db32b0cb3ac209a9e73f2794a6c
shch <shch91@126.com>
2021-04-18 13:31:38 +0800

    tree

--- shch/main/tree.go
+++ shch/main/tree.go
@@ -3,11 +3,53 @@ package main
 import "fmt"
 
 type TreeNode struct {
-	left  *TreeNode
-	right *TreeNode
-	val   int
+	Left  *TreeNode
+	Right *TreeNode
+	Val   int
 }
 
-func main()  {
+//判断是否存在路径
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return targetSum == root.Val
+	}
+	left := hasPathSum(root.Left, targetSum-root.Val)
+	right := hasPathSum(root.Right, targetSum-root.Val)
+	return left || right
+}
+
+//声明
+var path [][]int
+
+//求出所有路径
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	var curPath []int
+	//初始化
+	path = make([][]int, 0)
+	dfs(root, targetSum, curPath)
+	return path
+}
+
+func dfs(root *TreeNode, targetSum int, curPath []int) {
+	if root == nil {
+		return
+	}
+	curPath = append(curPath, root.Val)
+	if root.Left == nil && root.Right == nil && root.Val == targetSum {
+		var val []int
+		for _, v := range curPath {
+			val = append(val, v)
+		}
+		path = append(path, val)
+		return
+	}
+	dfs(root.Left, targetSum-root.Val, curPath)
+	dfs(root.Right, targetSum-root.Val, curPath)
+}
+
+func main() {
 	fmt.Println("fdsfads")
 }
