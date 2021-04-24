87ac01d8d1b2ec7ebf944bb0dedc41c7dcf84ee8
shch <shch91@126.com>
2021-04-24 17:04:47 +0800

    redis

--- shch/go.mod
+++ shch/go.mod
@@ -2,4 +2,9 @@ module shch
 
 go 1.15
 
-require github.com/jinzhu/gorm v1.9.16
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/jinzhu/gorm v1.9.16
+	github.com/onsi/ginkgo v1.16.1 // indirect
+	github.com/onsi/gomega v1.11.0 // indirect
+)
--- shch/redis/redis.go
+++ shch/redis/redis.go
@@ -1,3 +1,46 @@
 package main
 
+import (
+	"github.com/go-redis/redis"
+	"log"
+	"time"
+)
 
+func main() {
+	//连接服务器
+	redisClient:= redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // use default Addr
+		Password: "",               // no password set
+		DB:       0,                // use default DB
+	})
+
+	//心跳
+	pong, err := redisClient.Ping().Result()
+	log.Println(pong, err) // Output: PONG <nil>
+
+	//kv读写
+	err = redisClient.Set("key", "value", 1*time.Second).Err()
+	log.Println(err)
+
+	//获取过期时间
+	tm, err := redisClient.TTL("key").Result()
+	log.Println(tm)
+
+	val, err := redisClient.Get("key").Result()
+	log.Println(val, err)
+
+	val2, err := redisClient.Get("missing_key").Result()
+	if err == redis.Nil {
+		log.Println("missing_key does not exist")
+	} else if err != nil {
+		log.Println("missing_key", val2, err)
+	}
+
+	//不存在才设置 过期时间 nx ex
+	value, err := redisClient.SetNX("counter", 0, 1*time.Second).Result()
+	log.Println("setnx", value, err)
+
+	//Incr
+	result, err := redisClient.Incr("counter").Result()
+	log.Println("Incr", result, err)
+}
