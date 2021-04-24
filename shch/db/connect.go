09c5ca922622d942a182c79c9de021fcaee923c1
shch <shch91@126.com>
2021-04-24 16:56:40 +0800

    gorm

---
+++ shch/db/connect.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+// UserInfo 用户信息
+type UserInfo struct {
+	ID     uint
+	Name   string
+	Gender string
+	Hobby  string
+}
+
+func main() {
+	db, err := gorm.Open("mysql", "root:Yy&sh2021@(127.0.0.1:3306)/Test?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+	// 自动迁移
+	db.AutoMigrate(&UserInfo{})
+
+	u1 := UserInfo{1, "枯藤", "男", "篮球"}
+
+	// 创建记录
+	db.Create(&u1)
+
+	// 查询
+	var u = new(UserInfo)
+	db.First(u)
+	fmt.Printf("%#v\n", u)
+	var uu UserInfo
+	db.Find(&uu, "hobby=?", "足球")
+	fmt.Printf("%#v\n", uu)
+	// 更新
+	db.Model(&u).Update("hobby", "双色球")
+	// 删除
+	db.Delete(&u)
+}
--- shch/go.mod
+++ shch/go.mod
@@ -1,3 +1,5 @@
 module shch
 
 go 1.15
+
+require github.com/jinzhu/gorm v1.9.16
